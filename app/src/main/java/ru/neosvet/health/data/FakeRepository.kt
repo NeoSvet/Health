369c36f311b5e290a2278c6529e21977ccdf6c75
Neosvet <neosvet333@gmail.com>
2022-02-28 11:59:14 +0300

    improve Repository

--- app/src/main/java/ru/neosvet/health/data/FakeRepository.kt
+++ app/src/main/java/ru/neosvet/health/data/FakeRepository.kt
@@ -1,12 +1,30 @@
 package ru.neosvet.health.data
 
+import java.util.*
+
 class FakeRepository : Repository {
-    override suspend fun getList(): List<HealthEntity> = listOf(
-        HealthEntity("dsf", 1645777389833L, 137, 71, 59),
-        HealthEntity("ddh", 1645776009833L, 126, 67, 49),
-        HealthEntity("daw", 1645701389833L, 141, 64, 63),
-        HealthEntity("xgn", 1645700009833L, 127, 73, 58),
-        HealthEntity("sdf", 1645635389833L, 150, 83, 55),
-        HealthEntity("sdf", 1645633009833L, 129, 79, 57)
-    )
+    companion object {
+        private val list = mutableListOf<HealthEntity>(
+            HealthEntity("dsf", 1645777389833L, 137, 71, 59),
+            HealthEntity("ddh", 1645776009833L, 126, 67, 49),
+            HealthEntity("daw", 1645701389833L, 141, 64, 63),
+            HealthEntity("xgn", 1645700009833L, 127, 73, 58),
+            HealthEntity("sdf", 1645635389833L, 150, 83, 55),
+            HealthEntity("sdf", 1645633009833L, 129, 79, 57)
+        )
+    }
+
+    override suspend fun getList(): List<HealthEntity> = list
+
+    override suspend fun add(time: Long, highPressure: Int, lowPressure: Int, pulse: Int) {
+        list.add(
+            HealthEntity(
+                id = UUID.randomUUID().toString(),
+                time = time,
+                highPressure = highPressure,
+                lowPressure = lowPressure,
+                pulse = pulse
+            )
+        )
+    }
 }
--- app/src/main/java/ru/neosvet/health/data/Repository.kt
+++ app/src/main/java/ru/neosvet/health/data/Repository.kt
@@ -2,4 +2,5 @@ package ru.neosvet.health.data
 
 interface Repository {
     suspend fun getList(): List<HealthEntity>
+    suspend fun add(time: Long, highPressure: Int, lowPressure: Int, pulse: Int)
 }
