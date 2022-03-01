08f914ff47814cecd3c13dacc39702014273019a
Neosvet <neosvet333@gmail.com>
2022-03-01 15:30:44 +0300

    remove import

--- app/src/main/java/ru/neosvet/health/utils/Extensions.kt
+++ app/src/main/java/ru/neosvet/health/utils/Extensions.kt
@@ -3,7 +3,6 @@ package ru.neosvet.health.utils
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
-import ru.neosvet.health.R
 
 fun TextInputEditText.isCorrect(): Boolean {
     return text.toString().length > 1
